b0195d3a6ec0888958867c614dd74c79e3e289d3
Joao Marcelo <marcelooliveira004@outlook.com>
2024-08-19 23:10:03 -0300

    chore: add dotenv and config tables

---
+++ src/config/init.js
@@ -0,0 +1,17 @@
+const db = require ("../db");
+const tableQueries = require("../queries/tables");
+
+const init = async () => {
+    try {
+    await db.query(tableQueries.createUsers());
+    await db.query(tableQueries.createCategories());
+    await db.query(tableQueries.createFinances());
+    console.log("Successfully created tables");
+    await db.end();
+    return;
+    } catch (error) {
+    throw new Error("Error connecting to database",error);
+}
+};
+
+init();
